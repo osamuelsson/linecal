6fdd7097993b07f13196bf8c05ae9f515f9f018e
Olof Samuelsson <olof@s8n.se>
2017-09-03 14:52:47 +0200

    Make it pprint a list of days with weekdays as number

--- src/linecal/core.clj
+++ src/linecal/core.clj
@@ -1,7 +1,40 @@
 (ns linecal.core
+  (:use [clojure.pprint :as pprint])
   (:gen-class))
+
+;; TODO
+;; Add a number to weekday mapper
+;; Change this to a "gen"
+;; Add a "sum", etc.
+
+(defn createLine
+  "creates one of the lines"
+  [ ymd ]
+  (let [year (nth ymd 0)
+        month (nth ymd 1)
+        day (nth ymd 2)
+        cal (nth ymd 3)]
+    (.set cal year month day)
+    (let [dayOfWeek (.get cal java.util.Calendar/DAY_OF_WEEK)]
+      [year month day dayOfWeek])))
+
+(defn line-calendar
+  "Returns a list of the days in the month represented by the date and the day of week"
+  [year month]
+  (let [cal (.newInstance java.util.GregorianCalendar)
+        firstDayOfMonth (.getActualMinimum cal java.util.Calendar/DAY_OF_MONTH)
+        lastDayOfMonth (.getActualMaximum cal java.util.Calendar/DAY_OF_MONTH)
+        year (.get cal java.util.Calendar/YEAR)
+        month (+ 1 (.get cal java.util.Calendar/MONTH))
+        ]
+    (map createLine
+         (map (fn [day] (list year month day cal))
+              (range firstDayOfMonth (+ 1 lastDayOfMonth))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (= (count args) 2)
+    (println (line-calendar (first args) (nth args 1)))
+    (pprint (line-calendar "2017" "09"))))
