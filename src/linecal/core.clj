ef59d6b3bd7b152ced71b11203e7551c84086c2b
Olof Samuelsson <olof.samuelsson@jeppesen.com>
2017-09-04 17:42:36 +0200

    Add name of day, note bug.

--- src/linecal/core.clj
+++ src/linecal/core.clj
@@ -3,17 +3,27 @@
   (:gen-class))
 
 ;; TODO
-;; Add a number to weekday mapper
+;; Make the day-of-week work correctly .. now it starts at monday for sep 2017 !?
+;; Make it generate for something other than the current month (.newInstance ..)
 ;; Change this to a "gen"
 ;; Add a "sum", etc.
+
+(def nameOfDay {1 'M
+                2 'Ti
+                3 'O
+                4 'To
+                5 'F
+                6 'L
+                7 'S})
 
 (defn createLine
   "creates one of the lines"
   [ ymd ]
   (let [[year month day cal] ymd]
+    (.clear cal)
     (.set cal year month day)
     (let [dayOfWeek (.get cal java.util.Calendar/DAY_OF_WEEK)]
-      [year month day dayOfWeek])))
+      [year month day (nameOfDay dayOfWeek)])))
 
 (defn line-calendar
   "Returns a list of the days in the month represented by the date and the day of week"
