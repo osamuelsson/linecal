5e082063b65d6ae4b52ffb6b342e77fc7403742e
Olof Solomon-Samuelsson (oie111) <olof.samuelsson@teliacompany.com>
2019-12-03 14:32:02 +0100

    fix description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject linecal "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Fiddle with a line-based calendar"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
